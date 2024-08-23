028b69459f9aa092bfa6c592d85499e6170a79df
Adi Jain <jainadi313@gmail.com>
2024-08-23 19:59:55 +0530

    minor changes

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { CoverDemo } from '../../UI/CoverDemo'
+import { CardHoverEffectDemo } from '../../UI/CardHoverEffectDemo'
+
 const Achievements = () => {
   return (
     <div>
       <CoverDemo></CoverDemo>
+      <CardHoverEffectDemo></CardHoverEffectDemo>
       I am Achievements
     
     </div>
--- src/components/RecentActivities.jsx
+++ src/components/RecentActivities.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { SparklesPreview } from '../../UI/SparklesPreview'
 import { TypewriterEffectSmoothDemo } from '../../UI/TypewriterEffectSmoothDemo'
-
+import { ExpandableCardDemo } from './ExpandableCardDemo'
 const RecentActivities = () => {
   return (
     <div>
   <TypewriterEffectSmoothDemo></TypewriterEffectSmoothDemo>
  <SparklesPreview></SparklesPreview>
+ <ExpandableCardDemo></ExpandableCardDemo>
     </div>
   )
 }
