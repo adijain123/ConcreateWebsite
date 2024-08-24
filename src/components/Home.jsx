a2952a5b60cca587fce3ce614e7c75ea676198ee
Adi Jain <jainadi313@gmail.com>
2024-08-24 12:20:42 +0530

    minor changess

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { ImagesSliderDemo } from '../../UI/ImagesSliderDemo'
+import { InfiniteMovingCardsDemo } from '../../UI/InfiniteMovingCardsDemo'
 
 const Home = () => {
   return (
     <div>
       <ImagesSliderDemo></ImagesSliderDemo>
-   
- 
+      <InfiniteMovingCardsDemo></InfiniteMovingCardsDemo>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,43 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
+const colors = require("tailwindcss/colors")
+const {
+  default: flattenColorPalette
+} = require("tailwindcss/lib/util/flattenColorPalette")
+
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "./UI/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    // rest of the code
+    extend: {
+      animation: {
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite"
+      },
+      keyframes: {
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))"
+          }
+        }
+      }
+    }
   },
-  plugins: [],
+  plugins: [addVariablesForColors]
+}
+
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"))
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  )
+
+  addBase({
+    ":root": newVars
+  })
 }
