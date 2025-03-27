b004142fc7ee4963685006fda943d1ab7fa3df3b
Adi Jain <jainadi313@gmail.com>
2025-03-27 22:25:46 +0530

    achievements

--- UI/CardHoverEffectDemo.jsx
+++ UI/CardHoverEffectDemo.jsx
@@ -9,39 +9,39 @@ export function CardHoverEffectDemo() {
 }
 export const projects = [
   {
-    title: "Stripe",
+    title: "Civil Conclave 2025",
     description:
-      "A technology company that builds economic infrastructure for the internet.",
-    link: "https://stripe.com"
+      "Winner",
+    link: ""
   },
   {
-    title: "Netflix",
+    title: "Civil Conclave 2024",
     description:
-      "A streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.",
-    link: "https://netflix.com"
+      "Runner-up",
+    link: ""
   },
   {
-    title: "Google",
+    title: "Civil Conclave 2023",
     description:
-      "A multinational technology company that specializes in Internet-related services and products.",
-    link: "https://google.com"
+      "Winner",
+    link: ""
   },
   {
-    title: "Meta",
+    title: "Inter-IIT",
     description:
-      "A technology company that focuses on building products that advance Facebook's mission of bringing the world closer together.",
-    link: "https://meta.com"
+      "",
+    link: ""
   },
   {
-    title: "Amazon",
+    title: "Sustainability",
     description:
-      "A multinational technology company focusing on e-commerce, cloud computing, digital streaming, and artificial intelligence.",
-    link: "https://amazon.com"
+      "",
+    link: ""
   },
   {
-    title: "Microsoft",
+    title: "Awards",
     description:
-      "A multinational technology company that develops, manufactures, licenses, supports, and sells computer software, consumer electronics, personal computers, and related services.",
-    link: "https://microsoft.com"
+      "",
+    link: ""
   }
 ]
